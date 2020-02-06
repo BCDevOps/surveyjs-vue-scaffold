07ac933ab4857283693f9b0d82b0f447246bdbb4
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2020-02-06 14:56:51 -0800

    have 3 basic components

--- flapp-webapp/src/App.vue
+++ flapp-webapp/src/App.vue
@@ -2,16 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Survey msg="The survey goes here" />
+    <SurveyCreator msg="The survey creator goes here" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Survey from "./components/Survey.vue";
+import SurveyCreator from "./components/SurveyCreator.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    Survey,
+    SurveyCreator
   }
 };
 </script>
