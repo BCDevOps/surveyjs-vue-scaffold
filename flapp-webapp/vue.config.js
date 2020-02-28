972607541113dffdb4e60498fc9f0e95766608e1
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-02-27 16:58:10 -0800

    Wiring existing survey's
    -Ported Over custom widgets
    -Wired general information survey

--- flapp-webapp/vue.config.js
+++ flapp-webapp/vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   chainWebpack: config => {
     config.module.rules.delete("eslint");
+    config.module.rule('ts')
+    .test(/\.ts$/)
+    .use('ts-loader')
+    .loader('ts-loader')
+    .options({
+      appendTsSuffixTo: [/\.vue$/]
+    })
   },
   css: {
     loaderOptions: {
